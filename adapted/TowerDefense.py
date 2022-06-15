130b813e1ca8a5288fccf0e50af8a6ac7e3c0091
shivnaren <shivnaren@gmail.com>
2022-06-15 20:14:32 +0530

    Extract `button` ABC.

--- adapted/TowerDefense.py
+++ adapted/TowerDefense.py
@@ -9,6 +9,7 @@ from tkinter import *
 from PIL import Image, ImageDraw, ImageTk
 
 import helpers as H
+import buttons
 import display
 import grid
 import tower
@@ -323,29 +324,7 @@ class NextWaveButton:
         canvas.create_text(500, 37, text="Next Wave")
 
 
-class MyButton(object):
-    def __init__(self, x, y, xTwo, yTwo):
-        self.x = x
-        self.y = y
-        self.xTwo = xTwo
-        self.yTwo = yTwo
-
-    def checkPress(self, click, x, y):
-        if x >= self.x and y >= self.y and x <= self.xTwo and y <= self.yTwo:
-            self.pressed()
-            return True
-        return False
-
-    def pressed(self):
-        pass
-
-    def paint(self, canvas):
-        canvas.create_rectangle(
-            self.x, self.y, self.xTwo, self.yTwo, fill="red", outline="black"
-        )
-
-
-class TargetButton(MyButton):
+class TargetButton(buttons.MyButton):
     def __init__(self, x, y, xTwo, yTwo, myType):
         super(TargetButton, self).__init__(x, y, xTwo, yTwo)
         self.type = myType
@@ -355,9 +334,7 @@ class TargetButton(MyButton):
         displayTower.targetList = self.type
 
 
-class StickyButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(StickyButton, self).__init__(x, y, xTwo, yTwo)
+class StickyButton(buttons.MyButton):
 
     def pressed(self):
         global displayTower
@@ -367,9 +344,7 @@ class StickyButton(MyButton):
             displayTower.stickyTarget = False
 
 
-class SellButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(SellButton, self).__init__(x, y, xTwo, yTwo)
+class SellButton(buttons.MyButton):
 
     def pressed(self):
         global displayTower
@@ -379,9 +354,7 @@ class SellButton(MyButton):
         displayTower = None
 
 
-class UpgradeButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(UpgradeButton, self).__init__(x, y, xTwo, yTwo)
+class UpgradeButton(buttons.MyButton):
 
     def pressed(self):
         global money
---
+++ adapted/buttons.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class MyButton(ABC):
+    """Generic Button."""
+
+    def __init__(self, x, y, xTwo, yTwo):
+        self.x = x
+        self.y = y
+        self.xTwo = xTwo
+        self.yTwo = yTwo
+
+    def checkPress(self, click, x, y):
+        if x >= self.x and y >= self.y and x <= self.xTwo and y <= self.yTwo:
+            self.pressed()
+            return True
+        return False
+
+    @abstractmethod
+    def pressed(self) -> None:
+        """Implement press functionality."""
+
+    def paint(self, canvas):
+        canvas.create_rectangle(
+            self.x, self.y, self.xTwo, self.yTwo, fill="red", outline="black"
+        )
+
+
+def is_within_bounds(self, x: int, y: int) -> bool:
+    return self.x <= x <= self.xTwo and self.y <= y <= self.yTwo
