52909c755a721303c94ce20a0bdebee4edd12072
shivnaren <shivnaren@gmail.com>
2022-06-16 16:34:58 +0530

    Refactored `Block`.

--- lib/block.py
+++ lib/block.py
@@ -1,30 +1,25 @@
+from dataclasses import dataclass
 import grid
 
 
+@dataclass
 class Block:
-    def __init__(self, x: float, y: float, blockNumber: int, gridx: int, gridy: int):
-        self.loc = grid.Loc(x, y)
-        self.canPlace = True
-        self.blockNumber = blockNumber
-        self.grid = grid.Point(gridx, gridy)
-        self.image = None
+    loc: grid.Loc
+    blockNumber: int
+    grid: grid.Point
+    canPlace: bool
 
 
 class NormalBlock(Block):
-    def __init__(self, x, y, blockNumber, gridx, gridy):
-        super(NormalBlock, self).__init__(x, y, blockNumber, gridx, gridy)
+    ...
 
 
 class PathBlock(Block):
-    def __init__(self, x, y, blockNumber, gridx, gridy):
-        super(PathBlock, self).__init__(x, y, blockNumber, gridx, gridy)
-        self.canPlace = False
+    ...
 
 
 class WaterBlock(Block):
-    def __init__(self, x, y, blockNumber, gridx, gridy):
-        super(WaterBlock, self).__init__(x, y, blockNumber, gridx, gridy)
-        self.canPlace = False
+    ...
 
 
 def block_factory(x: float, y: float, block_num: int, gridx: int, gridy: int) -> Block:
@@ -34,4 +29,10 @@ def block_factory(x: float, y: float, block_num: int, gridx: int, gridy: int) ->
         WaterBlock,
     )
     BlockType = blocks[block_num]
-    return BlockType(x, y, block_num, gridx, gridy)
+
+    return BlockType(
+        grid.Loc(x, y),
+        block_num,
+        grid.Point(gridx, gridy),
+        canPlace=BlockType is NormalBlock,
+    )
